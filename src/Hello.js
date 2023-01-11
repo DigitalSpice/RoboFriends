c2fca66c413473ad98e22481c8e4babe3cb1a0b8
DigitalSpice <lmcurry93@hotmail.com>
2023-01-11 05:28:20 -0800

    used tachyon classNames (npm install tachyons)

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Hello.css';
 
 class Hello extends React.Component{
     render(){
         return(
-            <h1>Hello, world</h1>
+            <div className='f1 tc'>
+                <h1>Hello, world</h1>
+                <p>Welcome to React</p>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Hello from './Hello'
+import Hello from './Hello';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
